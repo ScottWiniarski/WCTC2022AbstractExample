8befae4addb1e6983afd8a21344f4ba525a56978
Scott Winiarski <Scott1w1@hotmail.com>
2022-09-21 21:04:22 -0500

    Added two new concrete classes and the methods in Program.cs

---
+++ Models/Snake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractExample.Models
+{
+    internal class Snake : Animal
+    {
+        public override void Sleep()
+        {
+            Console.WriteLine($"Appears to be sleeping, but with reptiles you never know.");
+        }
+        public override void MakeNoise()
+        {
+            Console.WriteLine($"It hisses and rattles its' tail.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,22 +7,38 @@ namespace AbstractExample
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Which animal do you want to hear?");
+            Console.WriteLine("Which animal do you want to hear?\n(1) Pig\n(2) Dog\n(3) Snake\n(4) Monkey");
             var choice = Console.ReadLine();
 
-            Animal animal = null;
+            Animal animal;
 
             if (choice == "1")
             {
                 animal = new Pig();
+                animal?.MakeNoise();
+                animal?.Sleep();
             }
             else if (choice == "2")
             {
                 animal = new Dog();
+                animal?.MakeNoise();
+                animal?.Sleep();
+            }
+            else if(choice == "3")
+            {
+                animal = new Snake();
+                animal?.MakeNoise();
+                animal?.Sleep();
+            }
+            else if( choice == "4")
+            {
+                animal = new Monkey();
+                animal?.MakeNoise();
+                animal?.Sleep();
             }
 
-            animal?.MakeNoise();
-            animal?.Sleep();
+            //animal?.MakeNoise();
+           // animal?.Sleep();
         }
     }
 }
